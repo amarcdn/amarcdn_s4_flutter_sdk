031216cf6eb6ddc6d2d170140e342532775b1e2c
nizamuddinshamrat <nshamrat7@gmail.com>
2023-02-12 01:08:34 +0600

    createBucket function added

--- lib/amarcdn.dart
+++ lib/amarcdn.dart
@@ -2,6 +2,9 @@ library amarcdn;
 
 import 'package:dio/dio.dart';
 
+import 'data/region_data.dart';
+import 'models/region.dart';
+
 class AmarCND {
   String apiKey;
   String apiSecretKey;
@@ -14,5 +17,38 @@ class AmarCND {
     required this.regionTitle,
   }) {
     _dio = Dio();
+  }
+
+  Future<Response> createBucket({
+    required String bucketName,
+    bool isPrivate = false,
+  }) async {
+    RegionModel regionModel = getRegionIdAsKey(regionTitle: regionTitle);
+
+    Map<String, dynamic> header = <String, String>{
+      'api-key': apiKey,
+      'api-secret-key': apiSecretKey,
+      'rid': regionModel.regionId,
+    };
+
+    FormData formData = FormData();
+    formData.fields.add(MapEntry('bucket', bucketName));
+    formData.fields.add(MapEntry('isPrivate', isPrivate ? 'YES' : 'NO'));
+    formData.fields.add(MapEntry('region', regionModel.regionKey));
+
+    Response response;
+
+    var url = 'https://${regionModel.regionUrl}/bucket/create';
+
+    try {
+      response = await _dio.post(
+        url,
+        data: formData,
+        options: Options(headers: header),
+      );
+      return response;
+    } on DioError catch (error) {
+      throw Exception(error.message);
+    }
   }
 }
