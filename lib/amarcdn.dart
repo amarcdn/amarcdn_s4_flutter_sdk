74b92484da3a4e294c16183cb23b28120540d04f
nizamuddinshamrat <nshamrat7@gmail.com>
2023-03-19 01:04:46 +0600

    upload file as chunk private method added

--- lib/amarcdn.dart
+++ lib/amarcdn.dart
@@ -1,5 +1,7 @@
 library amarcdn_s4;
 
+import 'dart:io';
+
 import 'package:dio/dio.dart';
 
 import 'data/region_data.dart';
@@ -12,7 +14,7 @@ class AmarCDN {
   late Dio _dio;
   late Map<String, dynamic> header;
   late RegionModel regionModel;
-
+  final chunkSize = 1024 * 1024; //1MB
   AmarCDN({
     required this.apiKey,
     required this.apiSecretKey,
@@ -88,6 +90,50 @@ class AmarCDN {
       return response;
     } on DioError catch (error) {
       throw Exception(error.message);
+    }
+  }
+
+  Stream<Response> _uploadFileAsChunk({
+    required String bucketName,
+    required String bucketId,
+    required File file,
+  }) async* {
+    final fileName = file.path.split('/').last;
+    final fileLength = await file.length();
+    final int chunkCount = (fileLength / chunkSize).ceil();
+    String attachId = '';
+
+    try {
+      Response? response;
+      for (int i = 0; i < chunkCount; i++) {
+        final start = i * chunkSize;
+        final end =
+            (start + chunkSize) > fileLength ? fileLength : start + chunkSize;
+
+        final streamList = file.openRead(start, end);
+        final int streamListLength = end - start;
+
+        FormData formData = FormData();
+        formData.fields.add(MapEntry('attachId', attachId));
+        formData.fields.add(MapEntry('bucket', bucketName));
+        formData.fields.add(MapEntry('bucketId', bucketId));
+        formData.fields.add(MapEntry('region', regionModel.regionKey));
+        formData.fields.add(MapEntry('totalChunk', '$chunkCount'));
+        formData.fields.add(MapEntry('currentChunkNo', '${i + 1}'));
+
+        final multitpartFile =
+            MultipartFile(streamList, streamListLength, filename: fileName);
+        formData.files.add(MapEntry('file', multitpartFile));
+        response = await _dio.post(
+            'https://global.amarcdn.com/file/upload-chunk',
+            data: formData,
+            options: Options(headers: header));
+
+        attachId = response.data['body']['attachId'];
+        yield response;
+      }
+    } catch (e) {
+      rethrow;
     }
   }
 }
